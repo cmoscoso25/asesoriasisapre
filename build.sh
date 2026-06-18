eb3e6dedb77f39c48e1fea80a7ccf9dfddc361e2
Cristian Moscoso <sistemas@mercenarios.cl>
2026-06-18 11:36:51 -0400

    build: crear superusuario automatico via env vars

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear superusuario si las variables están definidas
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser --no-input || true
+fi
